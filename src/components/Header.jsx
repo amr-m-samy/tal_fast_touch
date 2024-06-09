eb8dcb4ae2ea516e7e5db3bca6e3e675cd2e14c2
amr-m-samy <a.samy.cs@gmail.com>
2024-06-09 22:37:49 +0300

    second demo pregress bar mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,9 @@ import logo from "../assets/quiz-logo.png"
 export default function Header() {
   return (
     <header>
-      <img src={logo} alt="Logo" />
-      <h1>ReactQuiz</h1>
+      {// <img src={logo} alt="Logo" />
+      }
+      <h1>Tal Game</h1>
     </header>
   )
 }
